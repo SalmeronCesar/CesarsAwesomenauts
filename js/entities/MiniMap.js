38065d8b6868e1be969d96af873edfaeb96e4ee8
Cesar Salmeron <cesarsalmeron1990@gmail.com>
2015-04-27 23:36:19 -0700

    On 2nd Intermediate Milestone video 4

--- js/entities/MiniMap.js
+++ js/entities/MiniMap.js
@@ -1,3 +1,16 @@
 game.MiniMap = me.Entity.extend({
+    init: function(x, y, settings) {
+        this._super(me.Entity, "init", [x, y, (
+                image: "minimap",
+                width: 809,
+                height: 208,
+                spritewidth: "809",
+                spriteheight: "208",
+                getShape: function(){
+                    return (new me.Rect(0, 0, 809,)).toPolygon();
+                }
+                )]);
+    this.floating = true;
     
+    }
 });
